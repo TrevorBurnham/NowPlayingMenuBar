0eb80939026e4f4a113a881a04aad80d9fe25002
Trevor Burnham <trevorburnham@gmail.com>
2024-03-24 20:47:18 -0400

    Initial Commit

---
+++ NowPlayingMenuBar/NowPlayingMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingMenuBarApp.swift
+//  NowPlayingMenuBar
+//
+//  Created by Trevor Burnham on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayingMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
