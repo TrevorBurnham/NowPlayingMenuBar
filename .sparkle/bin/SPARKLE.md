2e0c6cfe8f47b34d8ca2e53516db4ed8dfc6c531
Trevor Burnham <trevorburnham@gmail.com>
2024-04-15 22:40:58 -0400

    Enable automatic updates with Sparkle

---
+++ .sparkle/bin/SPARKLE.md
@@ -0,0 +1,8 @@
+# Sparkle binaries
+
+Sparkle v2.6.0.
+
+How to find the binaries from Xcode:
+
+* Under "Package Dependencies", right-click Sparkle and click "Show in Finder".
+* Go up a directory, then into `artifacts/sparkle/Sparkle/bin`.
--- NowPlayingMenuBar/NowPlayingMenuBarApp.swift
+++ NowPlayingMenuBar/NowPlayingMenuBarApp.swift
@@ -4,11 +4,18 @@
 //
 
 import SwiftUI
+import Sparkle
 
 @main
 struct NowPlayingMenuBarApp: App {
   @ObservedObject var observer = NowPlayingObserver()
   @ObservedObject var launchAtLogin = LaunchAtLogin.observable
+    
+  private let updaterController: SPUStandardUpdaterController
+    
+  init() {
+    updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+  }
 
   var body: some Scene {
     MenuBarExtra {
